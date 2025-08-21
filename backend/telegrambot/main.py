d9b068b5e08c5f0113244a9485530fb664979a8b
Solanum-basilicus <crablegman@protonmail.com>
2025-08-21 17:53:13 +0200

    telegram bot: building requirements after deploy to fix includes

--- backend/telegrambot/main.py
+++ backend/telegrambot/main.py
@@ -5,6 +5,8 @@ from telegram import Update
 from bot_factory import build_app
 
 WEBHOOK_SECRET = os.environ.get("WEBHOOK_SECRET", "hook")  # e.g. a random slug
+SECRET_TOKEN = os.environ.get("HEADERS_SECRET_TOKEN")
+
 tg = build_app()
 app = FastAPI()
 
@@ -15,6 +17,8 @@ async def health():
 @app.post(f"/telegram/{WEBHOOK_SECRET}")
 async def telegram_webhook(request: Request):
     data = await request.json()
+    if request.headers.get("X-Telegram-Bot-Api-Secret-Token") != os.environ["SECRET_TOKEN"]:
+        return Response(status_code=403)
     update = Update.de_json(data, tg.bot)
     await tg.process_update(update)
     return Response(status_code=HTTPStatus.NO_CONTENT)
