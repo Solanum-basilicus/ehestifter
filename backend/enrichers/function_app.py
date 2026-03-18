bda23f68b08ef99a01805f2dfb391e3aab3b9322
Solanum-basilicus <crablegman@protonmail.com>
2026-03-18 13:44:09 +0100

    enrichers: fixed import for dispatch timer

--- backend/enrichers/function_app.py
+++ backend/enrichers/function_app.py
@@ -2,6 +2,7 @@ import azure.functions as func
 import logging
 from routes import register_all
 from timers.cleanup_runs import main as cleanup_runs_main
+from timers.dispatch_projections import main as dispatch_projections_main
 
 app = func.FunctionApp()
 
@@ -16,10 +17,8 @@ register_all(app)
 @app.schedule(schedule="0 0 18 * * *", arg_name="mytimer", run_on_startup=False, use_monitor=False)
 def cleanup_runs(mytimer: func.TimerRequest) -> None:
     cleanup_runs_main(mytimer)
-    # it was not tested on creation. If you see a lot of old queued runs in DB - will have to get back to it.
 
 @app.function_name(name="dispatch_projections")
 @app.schedule(schedule="0 */2 * * * *", arg_name="mytimer", run_on_startup=False, use_monitor=False)
 def dispatch_projections(mytimer: func.TimerRequest) -> None:
-    dispatch_projections_main(mytimer)    
-    
+    dispatch_projections_main(mytimer)
